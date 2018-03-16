1dc026129da7ccb82841a6791e362abfef05cd0b
Lucas Emanoel <lucase@tuta.io>
2018-03-16 16:19:14 -0300

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'lib/azpay.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'azpay.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'],
+        },
+      },
+    }],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
+};
