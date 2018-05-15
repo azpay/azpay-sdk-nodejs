57acbd8225050449f531e6fdb937a36219b3efdc
Kersul <joaohkfaria@gmail.com>
2018-05-15 16:50:47 -0300

    Adding new version, using webpack patterns and commonjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'lib/azpay.js'),
+  entry: path.resolve(__dirname, 'lib/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'azpay.js',
+    path: path.resolve(__dirname, 'dist/lib'),
+    filename: 'index.js',
+    library: '',
+    libraryTarget: 'commonjs',
   },
   module: {
     rules: [{
